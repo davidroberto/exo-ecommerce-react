e58159f7c139efe0975170b72d47159d02816cf3
David Robert <contact@david-robert.fr>
2023-11-08 10:36:54 +0100

    if + variable

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,19 @@
 function Header() {
-  const isUserLogged = false;
+  const isUserLogged = true;
+
+  const itemsInCart = 12;
 
   return (
     <header>
       <h1>Super ecommerce</h1>
       <nav>
         <ul>
+          {isUserLogged ? <li>David Robert</li> : <li>Veuillez vous connecter</li>}
+
           <li>Home</li>
           <li>Products</li>
 
-          {isUserLogged ? <li>David Robert</li> : <li>Veuillez vous connecter</li>}
+          {itemsInCart > 0 ? <li>Cart : {itemsInCart}</li> : <li>Pas d'item au panier</li>}
         </ul>
       </nav>
     </header>
